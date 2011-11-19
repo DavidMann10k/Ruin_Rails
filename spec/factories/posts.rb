fe63c1e7193047345b1719979128abadc21d5723
David A. Mann <davidmann10k@gmail.com>
2011-11-19 10:47:03 -0600

    removed uneccessary files

--- spec/factories/posts.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :post do
-    string ""
-    integer ""
-    integer ""
-  end
-end
--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :user do
-    string ""
-    string ""
-    boolean ""
-  end
-end
