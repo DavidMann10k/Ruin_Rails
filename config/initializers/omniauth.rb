9586a2adfcf72c8b36909add7babf14100a1b5a3
David A. Mann <davidmann10k@gmail.com>
2011-12-02 16:28:24 -0600

    added facebook secrets

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -8,6 +8,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
    provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'yahoo', :identifier => 'yahoo.com' 
    provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'myopenid', :identifier => 'myopenid.com'
    provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'aol', :identifier => 'openid.aol.com'
-   provider :facebook, 'APP_ID', 'APP_SECRET'
+   provider :facebook, '205256926221597', 'b89e38cb979ca36da8389d2b165b1bb9'
    provider :twitter, 'PVpMXvTFSzYRiuYHRunyIA', 'pUu00u4WC5EUTTHoNRLdHZgGU5wIB1dVpTEIBOGbhEM'
 end
