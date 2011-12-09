4c85905a606305ce43d0589d4a76ddca15e58b4c
David A. Mann <davidmann10k@gmail.com>
2011-12-09 16:31:45 -0600

    can post now

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
       if @post.save
         @post.topic.touch
         flash[:success] = "New post created successfully!"
-        redirect_to topic_path(@post.topic_id, @topic.posts.num_pages)
+        redirect_to topic_path(@post.topic_id)
       else
         flash[:error] = "Error in post creation process!"
         render 'new'
