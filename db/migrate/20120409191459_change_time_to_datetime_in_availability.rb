ef8a47a5a7cfc1a179c9899c8300f52389cc928a
David A. Mann <davidmann10k@gmail.com>
2012-04-09 21:17:52 -0500

    cleaning out old availability crap

--- db/migrate/20120409191459_change_time_to_datetime_in_availability.rb
+++
@@ -1,6 +0,0 @@
-class ChangeTimeToDatetimeInAvailability < ActiveRecord::Migration
-  def change
-    change_column :availabilities, :begin, :datetime
-    change_column :availabilities, :end, :datetime
-  end
-end
--- db/migrate/20120409233352_remove_day_from_availabilities.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDayFromAvailabilities < ActiveRecord::Migration
-  def change
-    remove_column :availabilities, :day
-  end
-end
